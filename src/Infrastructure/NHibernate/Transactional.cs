e3985e9305eeb36125253393f358df0ab34ea887
zaur <zaur.nadiraliev.228@gmail.com>
2019-07-14 01:47:38 +0300

    Небольшие правки Transactional

--- src/Infrastructure/NHibernate/Transactional.cs
+++ src/Infrastructure/NHibernate/Transactional.cs
@@ -3,15 +3,23 @@ using NHibernate;
 
 namespace app.Infrastructure.NHibernate
 {
+    /// <summary>
+    /// Умеет оборачивать делегат в транзакцию
+    /// </summary>
     public static class Transactional
     {
-        private static ITransaction Transaction { get; set; }
+        private static ITransaction Transaction { get; }
         
         static Transactional()
         {
             Transaction = NHibernateHelper.OpenSession().Transaction;
         }
 
+        /// <summary>
+        /// Обернет делегат в транзакцию и вернет результат выполнения делегата.
+        /// </summary>
+        /// <param name="function">Делегат, который произойдет внутри транзакции</param>
+        /// <typeparam name="T">Тип, который вернет делегат</typeparam>
         public static T Action<T>(Func<T> function)
         {
             try
@@ -32,7 +40,11 @@ namespace app.Infrastructure.NHibernate
             }
             catch (Exception)
             {
-                Transaction.Rollback();
+                if (!Transaction.WasRolledBack)
+                {
+                    Transaction.Rollback();
+                }
+                
                 throw;
             }
 
