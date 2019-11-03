99650693ee581854846ab7aef56b2fb7268dd74a
zaur <zaur.nadiraliev.228@gmail.com>
2019-11-03 23:08:25 +0300

    RoomController добавил GET /rooms. Добавил документацию ко всем endpoint'ам в этом контроллере

--- src/application/Http/RoomController.cs
+++ src/application/Http/RoomController.cs
@@ -3,6 +3,7 @@ using Application.CQS.Room.Command;
 using Application.CQS.Room.Input;
 using Application.CQS.Room.Output;
 using Application.CQS.Room.Query;
+using Common.Util;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,17 +12,26 @@ namespace Application.Http
     [Route("rooms")]
     public class RoomController : Controller
     {
-        [Authorize(Roles = "Manager")]
+        // POST /rooms
         [HttpPost]
+        [Authorize(Roles = "Manager")]
         public void CreateRoom([FromServices] CreateRoomCommand command, [FromBody] CreateRoomInput input)
         {
             command.Execute(input);
         }
 
+        // GET /rooms/{roomId}
         [Route("{roomId}")]
         public RoomOutput GetRoom([FromServices] GetRoomQuery query, Guid roomId)
         {
             return query.Execute(roomId);
+        }
+
+        // GET /rooms
+        public PaginatedData<RoomOutput> GetAllRooms([FromServices] GetAllRoomsQuery query,
+            [FromQuery] GetRoomInputFilter filter, [FromQuery] Pagination pagination)
+        {
+            return query.Execute(filter, pagination);
         }
     }
 }
