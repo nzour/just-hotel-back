b7a1561c18337f5050a96a3bba1cfae08f11b2a0
zaur <zaur.nadiraliev.228@gmail.com>
2019-11-03 22:58:53 +0300

    В GetRoomInputFilter добавил фильтрацию по полю RoomType

--- src/application/CQS/Room/Input/GetRoomInputFilter.cs
+++ src/application/CQS/Room/Input/GetRoomInputFilter.cs
@@ -7,12 +7,18 @@ namespace Application.CQS.Room.Input
     public class GetRoomInputFilter : IInputFilter<RoomEntity>
     {
         public bool? IsRented { get; set; }
+        public string? RoomType { get; set; }
 
         public IQueryable<RoomEntity> Process(IQueryable<RoomEntity> query)
         {
             if (null != IsRented)
             {
                 query = query.Where(room => IsRented == room.IsBusy);
+            }
+
+            if (null != RoomType)
+            {
+                query = query.Where(room => RoomType == room.RoomType);
             }
 
             return query;
